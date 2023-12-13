71fc9b45ce27eed44ab0810cbdabeb0ebf357eb0
danilogomes76 <danilo762410@gmail.com>
2023-12-12 22:03:48 -0300

    responsive Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="flex justify-center">
       <div className="w-[64rem] items-center lg:p-0 flex justify-center flex-col">
         <Header />
-        <section className="p-5 mt-5 lg:m-0">
+        <section className="p-5 mt-5 items-center flex flex-col lg:m-0">
           <Apresentation />
           <Message />
           <Projects />
