362729bde89ee724656a79fc8884dc28f38b7986
danilogomes76 <danilo762410@gmail.com>
2024-02-08 18:28:50 -0300

    add a og:image in metadata object with backgroundd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,10 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Danilo Gomes",
     description: "Bem vindo ao meu portfólio!",
-    images: ["https://danilo-gomes.online/daniloGomes-with-bg.svg"],
+    images: [
+      "https://danilo-gomes.online/daniloGomes-with-bg.svg",
+      "https://avatars.githubusercontent.com/u/102035834?v=4",
+    ],
   },
 };
 
