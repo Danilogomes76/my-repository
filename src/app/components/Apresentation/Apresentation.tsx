690cca424b9847a1ac38e7ff4d88e60779d46044
danilogomes76 <danilo762410@gmail.com>
2024-06-28 01:37:02 -0300

    Meteor animation

--- src/app/components/Apresentation/Apresentation.tsx
+++ src/app/components/Apresentation/Apresentation.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Header from "../Header/Header";
 import { LinkedinIcon, GithubIcon, CurriculumIcon } from "../SVGS/Svgs";
+import Meteors from "@/components/custom/meteors";
 
 const Apresentation: React.FC = () => {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32">
+    <section className="w-full relative  overflow-hidden  py-12 md:py-24 lg:py-32">
       <div className="container grid items-center gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-10">
         <div className="space-y-4">
           <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
@@ -18,12 +19,14 @@ const Apresentation: React.FC = () => {
           </p>
         </div>
 
+        <Meteors number={30} />
+
         <Image
           data-aos="fade-left"
           alt="Danilo Gomes Profile Image"
           width={400}
           height={400}
-          className="mx-auto bg-primary rounded-full"
+          className="mx-auto bg-muted rounded-full"
           src={"/dGomes.png"}
         />
       </div>
